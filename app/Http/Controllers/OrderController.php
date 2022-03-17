0e31b3aa5cf516edd7fcf96400d8e1be1cf54e0f
Eduardo Pessine <eduardopessine@gmail.com>
2022-03-16 22:47:28 -0300

    Create routes and controllers for orders

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function create()
+    {
+        return view('orders.create');
+    }
+
+    public function store(Request $request)
+    {
+        $rawArticles = $request->validate([
+            'articles' => 'required|array|min:1',
+            'articles.*' => 'required|array:ArticleCode,ArticleName,UnitPrice,Quantity',
+        ]);
+
+        $articles = collect();
+
+        collect($rawArticles)->each(
+            function ($rawArticle) use (&$articles) {
+                $article = Article::factory()->make([
+                    'code'       => data_get($rawArticle, 'ArticleCode'),
+                    'name'       => data_get($rawArticle, 'ArticleName'),
+                    'unit_price' => data_get($rawArticle, 'UnitPrice'),
+                    'quantity'   => data_get($rawArticle, 'Quantity'),
+                ]);
+
+                $articles->each(function (&$existingArticle) use (&$article, &$articles) {
+                    if ($existingArticle->code === $article->code) {
+                        $existingArticle->quantity++;
+                    } else {
+                        $articles->add($article);
+                    }
+                });
+            }
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::resource('orders', OrderController::class)->only(['create']);
